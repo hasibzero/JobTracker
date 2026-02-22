8be5763c99711a4698e4c90aff3605c75c18ef3b
Hasib <hasibsarkar98@gmail.com>
2026-02-23 02:39:48 +0600

    logic build done almost

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let availableList =[];
+let InterviewList =[];
 let rejectList =[];
 let currentStatus ='all';
 
@@ -11,4 +11,173 @@ let interviewBtn = document.getElementById('interview-btn');
 let rejectedBtn = document.getElementById('rejected-btn');
 
 const allCardSection = document.getElementById('allCards');
-const main = document.getElementById('allCards');
+const mainContainer = document.querySelector('main')
+const filterSection = document.getElementById('filtered-section')
+
+function calculateCount() {
+    total.innerText = allCardSection.children.length;
+    interviewCount.innerText = InterviewList.length;
+    rejectCount.innerText = rejectList.length;
+}
+calculateCount();
+
+function toggleStyle(id) {
+    allbtn.classList.add('btn-neutral/4', 'btn-outline');
+    interviewBtn.classList.add('btn-neutral/4', 'btn-outline');
+    rejectedBtn.classList.add('btn-neutral/4', 'btn-outline');
+
+    allbtn.classList.remove('btn-active','btn-info');
+    interviewBtn.classList.remove('btn-active','btn-info');
+    rejectedBtn.classList.remove('btn-active','btn-info');
+    
+    const selected = document.getElementById(id);
+    currentStatus =id;
+    
+    selected.classList.remove('btn-neutral/4', 'btn-outline');
+    selected.classList.add('btn-active','btn-info');
+    // rederInterview()
+
+    if (id=='interview-btn') {
+        allCardSection.classList.add('hidden');
+        filterSection.classList.add('hidden');
+        renderInterview();
+    }else if(id=='all-btn'){
+        allCardSection.classList.add('hidden');
+        filterSection.classList.add('hidden');
+    }
+    else if(id=='rejected-btn'){
+        allCardSection.classList.add('hidden');
+        filterSection.classList.add('hidden');
+        renderReject();
+    }
+}
+
+// 2
+mainContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('interviewClick')) {
+        const parentNode = event.target.parentNode.parentNode;
+        // console.log(parentNode)
+        const companyName = parentNode.querySelector('.companyName').innerText;
+        const position = parentNode.querySelector('.position').innerText;
+        const location = parentNode.querySelector('.location').innerText;
+        const status = parentNode.querySelector('.statusChk').innerText;
+        const details = parentNode.querySelector('.details').innerText;
+
+        parentNode.querySelector('.statusChk').innerText = 'Interviewed';
+
+        const cardInfo = {
+            companyName,
+            position,
+            location,
+            status: 'Interviewed',
+            details
+        }
+
+        const companyExist = InterviewList.find(item => item.companyName == cardInfo.companyName);
+        if (!companyExist) {
+            InterviewList.push(cardInfo)
+        }
+
+        rejectList =rejectList.filter(item=> item.companyName != cardInfo.companyName);
+
+        if (currentStatus=='rejected-btn') {
+            renderReject();
+
+        }
+        calculateCount();
+
+
+        // console.log(companyName,position,location,status);
+        // calculateCount()
+    } else if(event.target.classList.contains('rejectClick')){
+        // console.log('clicked')
+         const parentNode = event.target.parentNode.parentNode;
+        // console.log(parentNode)
+        const companyName = parentNode.querySelector('.companyName').innerText;
+        const position = parentNode.querySelector('.position').innerText;
+        const location = parentNode.querySelector('.location').innerText;
+        const status = parentNode.querySelector('.statusChk').innerText;
+        const details = parentNode.querySelector('.details').innerText;
+
+         parentNode.querySelector('.statusChk').innerText = 'Rejected';
+
+        const cardInfo = {
+            companyName,
+            position,
+            location,
+            status: 'Rejected',
+            details
+        }
+
+        const companyExist = InterviewList.find(item => item.companyName == cardInfo.companyName);
+        if (!companyExist) {
+            rejectList.push(cardInfo)
+        }
+
+        InterviewList =InterviewList.filter(item=> item.companyName != cardInfo.companyName);
+
+        if (currentStatus=='interview-btn') {
+            renderInterview();
+
+        }
+        calculateCount();
+    }
+})
+
+
+
+
+
+
+function renderInterview() {
+    filterSection.innerHTML = ``;
+
+    for (const interview of InterviewList) {
+        // console.log(interview)
+        let div = document.createElement('div');
+        div.className = 'p-6 bg-white rounded-[8px] border-2 border-[#f1f2f4]'
+        div.innerHTML = `
+                <div class="flex justify-between">
+                    <h4 class="companyName text-[18px] font-semibold ">${interview.companyName}</h4>
+                    <button id="dlt-btn" class="btn btn-soft btn-error btn-circle"><i class="fa-solid fa-trash"></i></button>
+                </div>
+                <p class="position text-gray-400">${interview.position}</p>
+                <p class="location text-gray-400 my-5">${interview.location}</p>
+                <p class="px-3 py-2 bg-[#eef4ff] inline-block my-2 statusChk">${interview.status}</p>
+                <p class="mb-5 text-[14px] details">${interview.details}</p>
+                <div>
+                    <button class="interviewClick btn btn-outline btn-success" disabled>INTERVIEW</button>
+                    <button class="rejectClick btn btn-outline btn-error">REJECTED</button>
+                </div>
+        `
+        filterSection.appendChild(div)
+    }
+}
+
+
+
+
+function renderReject() {
+    filterSection.innerHTML = ``;
+
+    for (const reject of rejectList) {
+        // console.log(interview)
+        let div = document.createElement('div');
+        div.className = 'p-6 bg-white rounded-[8px] border-2 border-[#f1f2f4]'
+        div.innerHTML = `
+                <div class="flex justify-between">
+                    <h4 class="companyName text-[18px] font-semibold ">${reject.companyName}</h4>
+                    <button id="dlt-btn" class="btn btn-soft btn-error btn-circle"><i class="fa-solid fa-trash"></i></button>
+                </div>
+                <p class="position text-gray-400">${reject.position}</p>
+                <p class="location text-gray-400 my-5">${reject.location}</p>
+                <p class="px-3 py-2 bg-[#eef4ff] inline-block my-2 statusChk">${reject.status}</p>
+                <p class="mb-5 text-[14px] details">${reject.details}</p>
+                <div>
+                    <button class="interviewClick btn btn-outline btn-success" disabled>INTERVIEW</button>
+                    <button class="rejectClick btn btn-outline btn-error" disabled>REJECTED</button>
+                </div>
+        `
+        filterSection.appendChild(div)
+    }
+}
