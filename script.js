001b70ede8fcc5963fb7df2e7e1ffa6d2fca92cc
Hasib <hasibsarkar98@gmail.com>
2026-02-23 00:34:31 +0600

    initial script

---
+++ script.js
@@ -0,0 +1,14 @@
+let availableList =[];
+let rejectList =[];
+let currentStatus ='all';
+
+let total = document.getElementById('total');
+let interviewCount = document.getElementById('interviewCount');
+let rejectCount = document.getElementById('rejectCount');
+
+let allbtn = document.getElementById('all-btn');
+let interviewBtn = document.getElementById('interview-btn');
+let rejectedBtn = document.getElementById('rejected-btn');
+
+const allCardSection = document.getElementById('allCards');
+const main = document.getElementById('allCards');
